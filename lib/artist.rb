1a61161d5a199dc88371b3407bf0f999fbbb1524
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-26 04:10:48 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,6 +12,10 @@ class Artist
   end
 
   def new_song(name, genre)
-  
+    song = Song.new(name, self, genre)
+    @songs << song
+    @genres << genre if !@genres.include?(genre)
+    song
   end
+
 end
